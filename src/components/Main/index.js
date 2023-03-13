56511d6bd7edef2f1da8f72aad56676bd5f8bf2b
maitrecanard <mathieusiaudeau@gmail.com>
2023-03-13 07:53:58 +0100

    main page

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,14 @@ import './main.scss';
 function Main() {
     return (
         <main>
-
+            <div className='main__presentation'>
+                <img src="" />
+                <div className='main__presentation__content'>
+                    <h1>Une écurie sur mesure</h1>
+                    <p>
+                    </p>
+                </div>
+            </div>
         </main>
     )
 }
