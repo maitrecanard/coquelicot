368cfe4947b97d33318b75ff57b4d2628cb06f36
maitrecanard <mathieusiaudeau@gmail.com>
2023-03-15 20:53:47 +0100

    fix display background

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,15 @@ import fond from '../../assets/images/fond.jpg';
 function Header() {
     return (
       <header>
-         <nav></nav>
+         <nav>
+          <ul>
+            <li>Accueil</li>
+            <li>Notre métier</li>
+            <li>Nos Services</li>
+            <li>Où sommmes nous ?</li>
+            <li>Nous contacter</li>
+          </ul>
+         </nav>
          <div className='ban' style={{ backgroundImage: `url(${fond})`, backgroundPosition: '150px' }}>
             <h1>Pension le <span className='coquelicot'>Coquelicot</span></h1>
          </div>
