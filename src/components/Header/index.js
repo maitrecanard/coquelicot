9a079e2334e07c20fe24ea5b2a09df93ad586772
maitrecanard <mathieusiaudeau@gmail.com>
2023-03-18 21:31:25 +0100

    test react responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,19 @@
 import './header.scss'
 import fond from '../../assets/images/fond.jpg'; 
+import fondmob from '../../assets/images/fondmob.jpg'; 
 import wood from '../../assets/images/wood.jpg';
+import { useMediaQuery } from 'react-responsive'
 
 function Header() {
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+    const isDesktop = useMediaQuery({ query: '(min-width: 769px)' });
     return (
       <header style={{ backgroundImage: `url(${wood})`, 
                        backgroundPosition: 'top center', 
                        backgroundRepeat: 'no-repeat',
                        backgroundSize:'cover',
                        backgroundAttachment: 'fixed'}}>
-         <nav>
+       { isDesktop && <nav>
           <ul>
             <li><a href="#">Accueil</a></li>
             <li><a href="">Notre métier</a></li>
@@ -17,10 +21,13 @@ function Header() {
             <li><a href="">Où sommmes nous ?</a></li>
             <li><a href="">Nous contacter</a></li>
           </ul>
-         </nav>
-         <div className='ban' style={{ backgroundImage: `url(${fond})`, backgroundPosition: '150px' }}>
+         </nav> }
+       { isDesktop &&  <div className='ban' style={{ backgroundImage: `url(${fond})`, backgroundPosition: '150px' }}>
             <h1>Pension le <span className='coquelicot'>Coquelicot</span></h1>
-         </div>
+         </div> }
+         { isMobile &&  <div className='ban' style={{ backgroundImage: `url(${fondmob})` }}>
+            <h1>Pension le <span className='coquelicot'>Coquelicot</span></h1>
+         </div> }
       </header>
     );
   }
